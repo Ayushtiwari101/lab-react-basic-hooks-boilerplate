50eff2e30ecf4b4e4825e30ad0775e9eba20f171
Ayushtiwari101 <ayush.tiwari@kalvium.community>
2024-01-13 18:23:08 +0530

    added react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import React, { useState } from 'react'
+import UseState from './components/UseState'
+import UseEffect from './components/UseEffect'
+import UseContext  from './components/UseContext'
+
+export const ToggleTheme = React.createContext()
+
+function App() {
+
+  const [state, setState] = useState(false)
+  const handleToggle =() =>{
+    setState(state=> !state)
+  }
+
+
+  return (
+    <>
+      <UseState/>
+      <UseEffect/>
+
+      <ToggleTheme.Provider value={state}>
+        <button onClick={handleToggle}>Toggle</button>
+        <UseContext/>
+      </ToggleTheme.Provider>
+    </>
+  )
+}
+
+export default App
